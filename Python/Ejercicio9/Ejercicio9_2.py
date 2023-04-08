8dcdd300dab42d422f3315bcd8cfdd77ca94a3eb
johan herrera <johanherrera797@gmail.com>
2023-04-07 20:44:42 -0500

    Ejercicio_9

--- Python/Ejercicio9/Ejercicio9_2.py
+++ Python/Ejercicio9/Ejercicio9_2.py
@@ -11,6 +11,15 @@ def filtrado(num):
     return False
 
 filtro = filter(filtrado, lista_numeros)
+"""En Python, un objeto iterable se agota cuando se ha recorrido
+ completamente y no se puede volver a iterar sobre él. Esto se debe
+   a que los objetos iterables sólo tienen un elemento siguiente al 
+   que se puede acceder si su valor no es igual a None1. Si intenta 
+   iterar sobre un objeto None, encontrará el TypeError: “NoneType” 
+   el objeto no es iterable error1.
 
+Si desea volver a iterar sobre un objeto iterable en Python, debe
+ crear una nueva instancia del objeto iterable o convertirlo en 
+ una lista o tupla"""
 suma = reduce(lambda x, y: x + y, list(filtro))
 print(f"la suma es {suma}")
